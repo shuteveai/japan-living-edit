93e520f8e81dfc62e842ac3d0904067e9554b07d
shuteveai <shuteveai@users.noreply.github.com>
2026-09-02 12:23:41 +0900

    Use shared affiliate product component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://japanlivingedit.com',
   output: 'static',
-  integrations: [sitemap()],
+  integrations: [mdx(), sitemap()],
 });
