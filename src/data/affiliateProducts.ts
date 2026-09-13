f0513df412383bd54e5736f4ede90334a351238b
shuteveai <shuteveai@users.noreply.github.com>
2026-09-13 17:47:14 +0900

    Fix affiliate source attribution issues

--- src/data/affiliateProducts.ts
+++ src/data/affiliateProducts.ts
@@ -101,15 +101,6 @@ export const affiliateProducts = {
     amazonJpUrl:
       'https://www.amazon.co.jp/%E5%B2%A9%E9%8B%B3-Iwachu-7%E5%9E%8B%E3%82%A2%E3%83%A9%E3%83%AC-IH%E5%AF%BE%E5%BF%9C-11720/dp/B091BS9VMM?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=3RI7BRYJAM67V&dib=eyJ2IjoiMSJ9.1e-M-kUl4Xxybex6tVxEkjhI_8IS7WsVTpnwPEDOVOYyYD0TUo1GDsHytdBwBtRH-DWjSsCXJuTbNMnLhl-vHMhlCcQOoclh-Q-vnvwHQMquh8uT1IvVARQb_vjws8o5zTnb-l7thSPsvV2jfd6N-8Id6DDNChz1mVlrXS7PQ_Siz5BuSGHfQX6s6db4rBPKIMcTq0h3uj8VEu9v2_6WwhctVqABEDWYIyNQcIYiMFduZP1k2h-_j7Tb0_Bl4LFmcIhN9dt2rn4-Bwuqrw1ffBzkcq4Zmk6425Ip1D8F6-c.42wnT2q1pkdvKLumrA2JhPJKLAAFIx51NK23d9yPHGg&dib_tag=se&keywords=%E5%B2%A9%E9%8B%B3%2B%E9%89%84%E7%93%B6%2B7%E5%9E%8B%E3%82%A2%E3%83%A9%E3%83%AC%2BIH&qid=1788324454&sprefix=%E5%B2%A9%E9%8B%B3%2B%E9%89%84%E7%93%B6%2B7%E5%9E%8B%E3%82%A2%E3%83%A9%E3%83%AC%2Bih%2Caps%2C330&sr=8-1&ufe=app_do%3Aamzn1.fos.35785624-70c4-44ae-a5c3-3f044f475d63&th=1&linkCode=ll2&tag=japanlivinged-22&linkId=271dfaf5255f36ff71420f774283820f&ref_=as_li_ss_tl',
   },
-  pilotCustom823TransparentBlackFine: {
-    key: 'pilotCustom823TransparentBlackFine',
-    displayName: 'PILOT CUSTOM 823, Transparent Black, Fine',
-    manufacturer: 'PILOT',
-    model: 'FKK-3MRP-TBF',
-    asin: 'B001AXB956',
-    amazonJpUrl:
-      'https://www.amazon.co.jp/%E3%83%91%E3%82%A4%E3%83%AD%E3%83%83%E3%83%88-%E4%B8%87%E5%B9%B4%E7%AD%86-%E3%82%AB%E3%82%B9%E3%82%BF%E3%83%A0823-FKK3MRPBNF-%E3%83%96%E3%83%A9%E3%82%A6%E3%83%B3%E8%BB%B8/dp/B001AXB956?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=AW1GV9CV50J0&dib=eyJ2IjoiMSJ9.rji8eN9h_iA35Za35XD8-q6l8raawSjlNdyIV0F-frzJ4TjAV-vhGJZfcYLVjvdR.B1YW0i_xiIEuN_6AN3P4nCCAkDy55GwlwJUpfORc72g&dib_tag=se&keywords=PILOT%2B%E3%82%AB%E3%82%B9%E3%82%BF%E3%83%A0823%2B%E7%B4%B0%E5%AD%97&qid=1788323105&sprefix=pilot%2B%E3%82%AB%E3%82%B9%E3%82%BF%E3%83%A0823%2B%E7%B4%B0%E5%AD%97%2Caps%2C227&sr=8-1&th=1&linkCode=ll2&tag=japanlivinged-22&linkId=b032f92c04f5a4f8b4250d845a17a150&ref_=as_li_ss_tl',
-  },
   harioV60CeramicDripper02White: {
     key: 'harioV60CeramicDripper02White',
     displayName: 'HARIO V60 Ceramic Coffee Dripper, Size 02, White',
